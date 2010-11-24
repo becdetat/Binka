8d794930ebb133a1ff01cb4dac321218eb6b76d0
Ben Scott <ben@belfryimages.com.au>
2010-11-24 22:17:54 +1000

    implemented front page view

--- app/views/binka/index.php
+++ app/views/binka/index.php
@@ -1,14 +1,14 @@
 <h1>Landing page</h1>
 
 <?php foreach ($posts as $post) { ?>
-	<h2><?php eh($post['title); ?></h2>
-	<h3><?php e(date('d F Y', $posted)); ?></h3>
-	<?php pr($tags); ?>
+	<h2><?php eh($post['title']); ?></h2>
+	<h3><?php e(date('d F Y', $post['posted'])); ?></h3>
+	<?php pr($post['tags']); ?>
 	<h4>
-		<?php for($i = 0; $i < count($tags); $i ++) {?>
-			|<?php eh($tags[$i]); ?>|<?php if ($i < count($tags) - 1) e(','); ?>
+		<?php for($i = 0; $i < count($post['tags']); $i ++) {?>
+			|<?php eh($post['tags'][$i]); ?>|<?php if ($i < count($post['tags']) - 1) e(','); ?>
 		<?php } ?>
 	</h4>
-	<?php e($post); ?>
+	<?php e($post['post']); ?>
 	<hr/>
 <?php } ?>
